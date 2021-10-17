30261c0431b1dad5fe759b3123da5fd62a13ba64
IlyaFrontJs <ilyafazylbaev@mail.ru>
2021-10-17 13:47:29 +0600

    HTML coding

---
+++ WebSite/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    let a = 'Text';
+
+    $(window).scroll(function () {
+        let top = $(document).scrollTop();
+
+        if(top >= 60){
+            $('.menu').addClass('fixed');
+        }else{
+            $('.menu').removeClass('fixed');
+        }
+    });
+
+    $('.burger').click(function (e) {
+        e.preventDefault();
+        $('.menuListTop').addClass('active');
+    });
+
+})
